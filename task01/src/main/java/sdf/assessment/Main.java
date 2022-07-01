32ff70723274c3de5f08afdec640a3ab7fa6a6bc
kevinrr123 <kevin.richard.r.7@gmail.com>
2022-07-01 12:12:30 +0800

    task01 halfway

--- task01/src/main/java/sdf/assessment/Main.java
+++ task01/src/main/java/sdf/assessment/Main.java
@@ -2,34 +2,34 @@ package sdf.assessment;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;  
 
-public class App 
+//"C:/Users/kevin/sdf-assessment/task01/static/thankyou.csv"
+
+public class Main
 {
 
     public static void main(String[] args) throws Exception  
     {
-        File f = new File("C:/Users/kevin/sdf-assessment/task01/static/thankyou.csv");   
+        
+        File f = new File("C:/Users/kevin/sdf-assessment/task01/static/thankyou.csv");
+        if(args.length>0)
+        f = new File(args[0]);   
         String line = "";  
-        List<String[]> content = new ArrayList<>();
         String[] tempArr;
-        FileWriter writer = new FileWriter("output.txt");
+        String newFile = args[1];
+        FileWriter writer = new FileWriter(newFile);
         try{
         BufferedReader br = new BufferedReader(new FileReader(f));  
         while ((line = br.readLine()) != null)   //returns a Boolean value  
             {  
-                content.add(line.split(","));
                 tempArr = line.split(",");
                 //System.out.println(content);
-                for (String str : tempArr) {
-                writer.write(str + " ");
-                }
-                writer.write("\n");
+                
+                writer.write(tempArr[2] + "\n\n" + "Dear " + tempArr[0] + "\n\n" + 
+                "Thank you for staying with us over these " + tempArr[3] + " years.\n\n");
             }
             writer.close();
                 //String[] item = line.split(",");    // use comma as separator  
